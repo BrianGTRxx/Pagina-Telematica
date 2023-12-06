7e0d89ff228f0c3b2f22b72750f72e64fda191d3
1-AndresVelez-1 <ingeniero.andresv.electronico09@gmail.com>
2023-12-05 23:29:54 -0500

    Cambios

--- src/controllers/buscar.controller.js
+++ src/controllers/buscar.controller.js
@@ -6,6 +6,7 @@ BuscarCtrl.BuscarComponente = async (req, res) => {
     try {
         const { nombreC } = req.body;
 
+        // Lógica para el primer sitio web
         const browser1 = await puppeteer.launch({ headless: 'new' });
         const page1 = await browser1.newPage();
 
@@ -13,37 +14,56 @@ BuscarCtrl.BuscarComponente = async (req, res) => {
 
         await page1.type('#search_query_top', nombreC);
         await page1.click('button');
+
         await page1.waitForSelector('#center_column');
 
         const componentPrice1 = await page1.$eval('span.price.product-price', el => el.textContent);
         const componentLink1 = await page1.$eval('#product_list > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.first-in-line.first-item-of-tablet-line.first-item-of-mobile-line > div > div.left-block > div > a', el => el.href);
-
+        
         console.log(`El precio más barato para ${nombreC} (Sitio 1) es: ${componentPrice1}`);
         console.log(`El link del componente para ${nombreC} (Sitio 1) es: ${componentLink1}`);
 
         await browser1.close();
 
+
         // Lógica para el segundo sitio web
         const browser2 = await puppeteer.launch({ headless: 'new' });
         const page2 = await browser2.newPage();
 
-        await page2.goto('https://www.bigtronica.com/', { waitUntil: 'domcontentloaded', timeout: 60000 });
-
+        await page2.goto('https://www.bigtronica.com/');
         await page2.type('#search_query_top', nombreC);
+
         await page2.evaluate(() => {
             document.querySelector('button[type="submit"]').click();
         });
+
         await page2.waitForSelector('#center_column');
 
         const componentPrice2 = await page2.$eval('span.price.product-price', el => el.textContent);
         const componentLink2 = await page2.$eval('#product_list > li.ajax_block_product.col-xs-12.col-sm-6.col-md-4.first-in-line.first-item-of-tablet-line.first-item-of-mobile-line > div > div.left-block > div > a', el => el.href);
-
+        
         console.log(`El precio más barato para ${nombreC} (Sitio 2) es: ${componentPrice2}`);
         console.log(`El link del componente para ${nombreC} (Sitio 2) es: ${componentLink2}`);
+
         await browser2.close();
 
-        // Puedes enviar los datos al cliente o renderizar la vista con los resultados aquí
-        res.render('principal/Componentes', { componentPrice1, componentLink1, componentPrice2, componentLink2 });
+        // Verificar si se encontraron resultados en ambos sitios
+        const resultadosSitio1 = componentPrice1 && componentLink1;
+        const resultadosSitio2 = componentPrice2 && componentLink2;
+
+        // Renderizar según los resultados
+        if (resultadosSitio1 || resultadosSitio2) {
+            res.render('principal/Componentes', {resultadosSitio1,
+                resultadosSitio2,
+                componentPrice1,
+                componentLink1,
+                componentPrice2,
+                componentLink2
+            });
+        } else {
+            res.render('principal/Componentes', { resultadosNulos: true });
+        }
+
     } catch (error) {
         console.error('Error al buscar componentes:', error);
         res.status(500).send('Error interno del servidor');
@@ -51,4 +71,3 @@ BuscarCtrl.BuscarComponente = async (req, res) => {
 };
 
 module.exports = BuscarCtrl;
-
